8d3c93f9089c11bff898d7405ee4ede1c4f60693
Konstantinos Thanaselos <kthanaselos@gmail.com>
2020-05-02 14:17:22 +0300

    Add project files.

---
+++ ModelCrm/Program.cs
@@ -0,0 +1,30 @@
+﻿using ModelCrm.Models;
+using ModelCrm.Options;
+using ModelCrm.Services;
+using System;
+
+namespace ModelCrm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerOptions custOps = new CustomerOptions()
+            {
+                FirstName = "Konstantinos",
+                LastName = "Thanaselos",
+                Address = "Efpalio",
+                VatNumber = "123456789",
+                Email = "kthanaselos@gmail.com",
+                Phone = "6976512900",
+                Dob = new DateTime(1994, 7, 17),
+            };
+
+            CustomerCrud custCrud = new CustomerCrud();
+            Customer customer = custCrud.CreateCustomer(custOps);
+
+            Console.WriteLine($"Id={customer.Id} " +
+                $"FirstName={customer.FirstName}");
+        }
+    }
+}
